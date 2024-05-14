9b6006d74f77b86fad9985f17dab511553ccf4e0
Albert Andrews <03jayteee@gmail.com>
2024-05-14 13:02:45 +0000

    ui kitten installaton

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { ApplicationProvider, Layout } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout style={styles.container}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </Layout>
+    </ApplicationProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "#ECEDEC",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
-    color: "white",
-    fontSize: 50,
+    fontSize: 20,
+    fontWeight: "bold", 
+    color: "#000000",
   }
-
 });
